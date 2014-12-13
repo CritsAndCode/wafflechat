1858febb6dd0a8d8a3e81d68a18c967f9650ab3e
Catlin Buckley <catlin.buckley@gmail.com>
2014-12-13 13:17:34 -0500

    Layout changes. Added logo images. Simplified UI.

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ var express = require('express'),
 
 var port = process.env.PORT || 8080;
 
-// Initialize a new socket.io object. It is bound to 
-// the express app, which allows them to coexist.
+// Initialize a new socket.io object
 
 var io = require('socket.io').listen(app.listen(port));
-
-// Require the configuration and the routes files, and pass
-// the app and io as arguments to the returned functions.
 
 require('./config')(app, io);
 require('./routes')(app, io);
